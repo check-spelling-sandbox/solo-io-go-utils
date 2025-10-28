7db07ce69ffb1d5f7a22381de3fdfc1cec48233c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-10-28 08:40:03 -0400

    spelling: split
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- contextutils/context_and_logging.go
+++ contextutils/context_and_logging.go
@@ -46,7 +46,7 @@ func buildProductionLogger() (*zap.Logger, error) {
 	config := zap.NewProductionConfig()
 	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 
-	// For non-splt logging, allow the user/environment to specify a log file location.
+	// For non-split logging, allow the user/environment to specify a log file location.
 	// When we log to a file, we will not log it STDOUT.
 	if os.Getenv(LogToFileLocationEnvName) != "" {
 		config.OutputPaths = []string{os.Getenv(LogToFileLocationEnvName)}
